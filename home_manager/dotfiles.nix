27c3d0ad5b01957752691bee42742385f5f4539c
jimmyff <code@rocketware.co.uk>
2025-07-05 13:35:30 +0100

    dotfiles

--- home_manager/dotfiles.nix
+++ home_manager/dotfiles.nix
@@ -6,10 +6,10 @@
     #};
 
     # vscode settings
-    home.file.".config/Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/vscode/.config/Code/User/settings.json;
-    home.file.".config/Code/User/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/vscode/.config/Code/User/keybindings.json;
+    home.file.".config/Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixfiles/dotfiles/vscode/.config/Code/User/settings.json";
+    home.file.".config/Code/User/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixfiles/dotfiles/vscode/.config/Code/User/keybindings.json";
 
     # cursor settings (vscode duplicated)
-    home.file.".config/Cursor/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/vscode/.config/Code/User/settings.json;
-    home.file.".config/Cursor/User/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/vscode/.config/Code/User/keybindings.json;
+    home.file.".config/Cursor/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixfiles/dotfiles/vscode/.config/Code/User/settings.json";
+    home.file.".config/Cursor/User/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixfiles/dotfiles/vscode/.config/Code/User/keybindings.json";
 }
