15e1b2d4429e86c7bd8ae5483ce37378644b8023
jimmyff <code@rocketware.co.uk>
2025-07-05 14:33:16 +0100

    chromium

--- home_manager/apps/chromium/chromium.nix
+++ home_manager/apps/chromium/chromium.nix
@@ -7,9 +7,17 @@
     config = lib.mkIf config.chromium_module.enable {
 
         home.packages = [
-            pkgs.chromium			# browser			
-            pkgs.google-chrome		# browser
+            #pkgs.google-chrome		# browser
         ];
+
+        programs.chromium = {
+            enable = true;
+            extensions = [
+                "nngceckbapebfimnlniiiahkandclblb" # bitwarden
+                "eljbmlghnomdjgdjmbdekegdkbabckhm" # Dart debug
+                "dbepggeogbaibhgnhhndojpepiihcmeb" # vimium
+            ];
+        };
 
         home.sessionVariables.BROWSER = "google-chrome-stable";
     };
