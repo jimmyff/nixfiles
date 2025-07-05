33b7df0364cd31fe7d921a9eda176b8e62ca635b
jimmyff <code@rocketware.co.uk>
2025-07-05 18:59:43 +0100

    docs

--- home_manager/apps/catppuccin.nix
+++ home_manager/apps/catppuccin.nix
@@ -1,27 +1,28 @@
 # Sway module
-{ inputs, pkgs, lib, config, ... }: {
-
-
+{
+  inputs,
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   imports = [
     inputs.catppuccin.homeModules.catppuccin
   ];
 
   options = {
-      catppuccin_module.enable = lib.mkEnableOption "enables catppuccin_module";
+    catppuccin_module.enable = lib.mkEnableOption "enables catppuccin_module";
   };
 
   config = lib.mkIf config.catppuccin_module.enable {
-
-
+    # for all options see: https://nix.catppuccin.com/search/v1.2/
     catppuccin.enable = true;
-    catppuccin.flavor = "mocha"; 
-    catppuccin.kitty.enable = true; 
+    catppuccin.flavor = "mocha";
+    catppuccin.kitty.enable = true;
     catppuccin.sway.enable = true;
     catppuccin.swaylock.enable = true;
 
     catppuccin.gtk.enable = true;
-    
-
   };
-
 }
+
