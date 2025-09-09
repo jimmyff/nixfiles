19adc8f96905876daa458767272c44a92cd9e153
jimmyff <code@rocketware.co.uk>
2025-09-09 22:57:27 +0100

    added atuin

--- home_manager/shared/apps/atuin/atuin.nix
+++ home_manager/shared/apps/atuin/atuin.nix
@@ -12,24 +12,6 @@
     programs.atuin = {
       enable = true;
       enableNushellIntegration = lib.mkIf config.programs.nushell.enable true;
-      
-      settings = {
-        # Basic atuin configuration
-        update_check = false;
-        sync_frequency = "5m";
-        sync_address = "https://api.atuin.sh";
-        auto_sync = true;
-        dialect = "us";
-        show_preview = true;
-        max_preview_height = 4;
-        show_help = true;
-        exit_mode = "return-original";
-        word_jump_mode = "word";
-        word_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        scroll_context_lines = 1;
-        history_format = "{{time}} :: {{command}}";
-        style = "compact";
-      };
     };
   };
 }
