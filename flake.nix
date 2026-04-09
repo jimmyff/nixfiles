08a9d717017bd982f9ac2bfc505c3193fe30b341
jimmyff <code@rocketware.co.uk>
2026-04-09 18:26:55 +0100

    minor fix for config file backups

--- flake.nix
+++ flake.nix
@@ -179,6 +179,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
           home-manager.extraSpecialArgs = darwinArgs;
           home-manager.users.${username} = import ./home/darwin;
         }
