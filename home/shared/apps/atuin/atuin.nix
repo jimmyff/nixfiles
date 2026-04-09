d28d70166285a652d5fdc650698e789f0a26e97d
jimmyff <code@rocketware.co.uk>
2026-04-09 18:15:39 +0100

    minor tweaks

--- home/shared/apps/atuin/atuin.nix
+++ home/shared/apps/atuin/atuin.nix
@@ -12,6 +12,12 @@
     programs.atuin = {
       enable = true;
       enableNushellIntegration = lib.mkIf config.programs.nushell.enable true;
+      settings = {
+        # Disable network call that pings api.atuin.sh to check for new releases
+        update_check = false;
+        # Skip storing commands matching built-in secret regexes (AWS keys, GH tokens, etc.)
+        secrets_filter = true;
+      };
     };
   };
 }
