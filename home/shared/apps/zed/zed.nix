4a376aa7767938d3ef1b02322d02fbef460a5d70
jimmyff <code@rocketware.co.uk>
2026-04-23 16:12:25 +0100

    added fhs zed for nix and updated pacakges

--- home/shared/apps/zed/zed.nix
+++ home/shared/apps/zed/zed.nix
@@ -11,7 +11,10 @@
   config = lib.mkIf config.zed_module.enable {
     programs.zed-editor = {
       enable = true;
-      package = pkgs-dev-tools.zed-editor;
+      package =
+        if pkgs-dev-tools.stdenv.isDarwin
+        then pkgs-dev-tools.zed-editor
+        else pkgs-dev-tools.zed-editor-fhs;
     };
     home.file.".config/zed".source = config.lib.file.mkOutOfStoreSymlink
       "${config.home.homeDirectory}/nixfiles/dotfiles/zed";
