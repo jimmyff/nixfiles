28ea15a4b4596c0b4c9cbcc27d020649215cdfa5
jimmyff <code@rocketware.co.uk>
2026-09-15 19:56:03 +0100

    fixes for nix build users

--- modules/workstation/desktop/environment_niri/greeter/greetd.nix
+++ modules/workstation/desktop/environment_niri/greeter/greetd.nix
@@ -11,6 +11,8 @@
         --time \
         --asterisks \
         --user-menu \
+        --user-menu-min-uid 1000 \
+        --user-menu-max-uid 29999 \
         --cmd "uwsm start -F -- /run/current-system/sw/bin/niri" \
         --theme "text=white;border=darkgray;action=gray;time=lightcyan;greet=lightcyan;prompt=lightyellow;input=lightmagenta"
     '';
